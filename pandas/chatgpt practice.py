e204eda8c9fe03f450c1b8ff1a70cd0ca1953ebc
20188948 <20188948@student.chelmsford.ac.uk>
2026-04-14 10:51:59 +0000

    epic.10

--- pandas/chatgpt practice.py
+++ pandas/chatgpt practice.py
@@ -1,4 +1,26 @@
-import pandas as pd 
-df=pd.read_csv('customers-1000.csv')
-counts=df["Country"].value_counts()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('/workspaces/codespaces-blank/customers-1000.csv')
+df.head()
+
+
+#print(pd.DataFrame(df))
+counts =df['Country'].value_counts()
+counts = counts[counts > 7].reset_index()
+counts.columns = ['Country', 'Count']
 print(counts)
+
+xpoints =df['Country']
+ypoints = df['City']
+
+plt.plot(xpoints,ypoints,'o')
+plt.xlabel('Country')
+plt.ylabel('City')
+plt.title('City by Country')
+
+
+print(plt.show())
+plt.savefig('Graph.png')
